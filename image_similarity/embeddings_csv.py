bcac16ebcea6b566cba07a30dddacdcc06b7ea94
anitabaral <baralanita2@gmail.com>
2021-08-21 16:50:59 +0545

    Changing code in embeddings_csv.py and images.py

--- image_similarity/embeddings_csv.py
+++ image_similarity/embeddings_csv.py
@@ -4,7 +4,8 @@ from .embeddings import Embedding
 
 class FeaturesCsv:
 
-  def ___init__(self):
+  def ___init__(self, path):
+    self.csv_path = path
     self.embeddings = Embedding.get_embeddings()
 
   def get_embeddings_dataframe(self):
@@ -12,6 +13,6 @@ class FeaturesCsv:
     total_embeddings_df = pd.DataFrame(columns=(np.arange(1, 4097)))
     for index, value in enumerate(self.embeddings):
       total_embeddings_df.loc[index] = value[0]
-    total_embeddings_df.to_csv('/content/Minor_project_5/data/embeddings.csv')
+    total_embeddings_df.to_csv(self.csv_path)
 
     return total_embeddings_df
--- image_similarity/images.py
+++ image_similarity/images.py
@@ -4,20 +4,19 @@ import pathlib as Path
 import cv2
 
 class LoadImage:
-  def __init__(self):
-    pass
+  def __init__(self, images_path):
+    self.path = images_path
 
   def get_images(self):
 
-    total_images = []
-    file_path = Path('/content/drive/MyDrive/Leapfrog_internship/Project 5/Project_5')
-    data_path = os.listdir(file_path)
+    total_images = []  
+    data_path = os.listdir(self.path)
     for sample in data_path:
-      image_path = file_path / sample
-      try: 
+      image_path = path / sample
+      if os.exists(image_path):
         image = cv2.imread(str(image_path))
         total_images.append(image)
-      except:
-        print('It is not an image')
+      else:
+        raise ValueError('Error while reading images')
     
     return total_images
