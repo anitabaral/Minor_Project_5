c7c91fcd5997cbd0ce154e3634e420f454d5ef13
anitabaral <baralanita2@gmail.com>
2021-08-21 16:47:51 +0545

    Adding path to dataset and file

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 from image_similarity import Embedding, FeaturesCsv, LoadImage, LoadModel
+
+import yaml
+
+with open("config.yaml", 'r') as stream:
+  file_paths = yaml.safe_load(stream)
+
+def main():
+   LoadImage(file_paths['images_loc'])
+   FeaturesCsv(file_paths['csv_loc'])
